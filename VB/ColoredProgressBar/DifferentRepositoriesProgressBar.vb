f4fc5bf08226ae5d316ae4815c9f11f767ddf0a1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ColoredProgressBar/DifferentRepositoriesProgressBar.vb
@@ -0,0 +1,78 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.XtraGrid.Columns
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraEditors.Repository
+Imports System.Drawing
+
+Namespace ColoredProgressBar
+	Friend Class DRProgressBarHelper
+		Private Column As GridColumn
+		Private View As GridView
+
+		Private prbLess25 As RepositoryItemProgressBar
+		Private prbLess50 As RepositoryItemProgressBar
+		Private prbLess75 As RepositoryItemProgressBar
+		Private prbLess100 As RepositoryItemProgressBar
+
+		Public Sub New(ByVal column As GridColumn)
+			PrbInit()
+			Me.Column = column
+			View = TryCast(Me.Column.View, GridView)
+			AddHandler View.CustomRowCellEdit, AddressOf View_CustomRowCellEdit
+
+		End Sub
+		Private Sub PrbInit()
+			prbLess25 = New RepositoryItemProgressBar()
+			prbLess25.StartColor = Color.Red
+			prbLess25.EndColor = Color.Red
+			prbLess25.ShowTitle = True
+			prbLess25.ProgressViewStyle = DevExpress.XtraEditors.Controls.ProgressViewStyle.Solid
+			prbLess25.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat
+			prbLess25.LookAndFeel.UseDefaultLookAndFeel = False
+
+			prbLess50 = New RepositoryItemProgressBar()
+			prbLess50.StartColor = Color.Orange
+			prbLess50.EndColor = Color.Orange
+			prbLess50.ShowTitle = True
+			prbLess50.ProgressViewStyle = DevExpress.XtraEditors.Controls.ProgressViewStyle.Solid
+			prbLess50.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat
+			prbLess50.LookAndFeel.UseDefaultLookAndFeel = False
+
+			prbLess75 = New RepositoryItemProgressBar()
+			prbLess75.StartColor = Color.YellowGreen
+			prbLess75.EndColor = Color.YellowGreen
+			prbLess75.ShowTitle = True
+			prbLess75.ProgressViewStyle = DevExpress.XtraEditors.Controls.ProgressViewStyle.Solid
+			prbLess75.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat
+			prbLess75.LookAndFeel.UseDefaultLookAndFeel = False
+
+			prbLess100 = New RepositoryItemProgressBar()
+			prbLess100.StartColor = Color.Green
+			prbLess100.EndColor = Color.Green
+			prbLess100.ShowTitle = True
+			prbLess100.ProgressViewStyle = DevExpress.XtraEditors.Controls.ProgressViewStyle.Solid
+			prbLess100.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat
+			prbLess100.LookAndFeel.UseDefaultLookAndFeel = False
+
+		End Sub
+
+		Private Sub View_CustomRowCellEdit(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.CustomRowCellEditEventArgs)
+			If e.Column Is Column Then
+				Dim percent As Integer = Convert.ToInt16(e.CellValue)
+				If percent < 25 Then
+					e.RepositoryItem = prbLess25
+				ElseIf percent < 50 Then
+					e.RepositoryItem = prbLess50
+				ElseIf percent < 75 Then
+					e.RepositoryItem = prbLess75
+				ElseIf percent <= 100 Then
+					e.RepositoryItem = prbLess100
+				End If
+			End If
+		End Sub
+
+	End Class
+End Namespace
---
+++ VB/ColoredProgressBar/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports DevExpress.XtraEditors.Drawing
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.Utils
+
+Namespace ColoredProgressBar
+	Partial Public Class Form1
+		Inherits Form
+
+		Private dt As New DataTable()
+		Public Sub New()
+			InitializeComponent()
+			dt.Columns.Add("Column")
+			For i As Integer = 0 To 100 Step 10
+				dt.Rows.Add(i)
+			Next i
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			gridControl1.DataSource = dt
+			Dim drHelper As New DRProgressBarHelper(col5)
+			Dim srHelper As New SRProgressBarHelper(col6)
+		End Sub
+
+		Private Sub gridView1_CustomDrawCell(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs) Handles gridView1.CustomDrawCell
+
+			If (e.Column Is col3) OrElse (e.Column Is col4) Then
+				DrawFocusRect(sender, e)
+				e.Handled = True
+			End If
+			If (e.Column Is col3) OrElse (e.Column Is col4) OrElse (e.Column Is col1) OrElse (e.Column Is col2) Then
+				DrawProgressBar(e)
+			End If
+			If e.Column Is col4 Then
+				DrawEditor(e)
+				e.Handled = True
+			End If
+		End Sub
+
+		Private Sub DrawFocusRect(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs)
+			Dim cell As GridCellInfo = TryCast(e.Cell, GridCellInfo)
+			e.Appearance.DrawBackground(e.Cache, e.Bounds)
+			Dim isFocusCell As Boolean = (cell.State And GridRowCellState.FocusedCell) <> 0 AndAlso (cell.State And GridRowCellState.GridFocused) <> 0
+			If isFocusCell AndAlso (TryCast(sender, GridView)).FocusRectStyle = DrawFocusRectStyle.CellFocus Then
+				e.Cache.Paint.DrawFocusRectangle(e.Graphics, cell.Bounds, cell.Appearance.GetForeColor(), cell.Appearance.GetBackColor())
+			End If
+		End Sub
+		Private Sub DrawEditor(ByVal e As DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs)
+			Dim cell As GridCellInfo = TryCast(e.Cell, GridCellInfo)
+			Dim offset As Point = cell.CellValueRect.Location
+			Dim pb As BaseEditPainter = TryCast(cell.ViewInfo.Painter, BaseEditPainter)
+			Dim savedStyle As AppearanceObject = cell.ViewInfo.PaintAppearance
+			If Not offset.IsEmpty Then
+				cell.ViewInfo.Offset(offset.X, offset.Y)
+			End If
+			Try
+				pb.Draw(New ControlGraphicsInfoArgs(cell.ViewInfo, e.Cache, cell.Bounds))
+			Finally
+				If Not offset.IsEmpty Then
+					cell.ViewInfo.Offset(-offset.X, -offset.Y)
+				End If
+			End Try
+
+		End Sub
+		Private Sub DrawProgressBar(ByVal e As DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs)
+			Dim percent As Integer = Convert.ToInt16(e.CellValue)
+
+			Dim v As Integer = Convert.ToInt32(e.CellValue)
+			v = v * e.Bounds.Width \ 100
+			Dim rect As New Rectangle(e.Bounds.X, e.Bounds.Y, v, e.Bounds.Height)
+			Dim b As Brush = Brushes.Green
+			If percent < 25 Then
+				b = Brushes.Red
+			ElseIf percent < 50 Then
+				b = Brushes.Orange
+			ElseIf percent < 75 Then
+				b = Brushes.YellowGreen
+			End If
+			e.Graphics.FillRectangle(b, rect)
+
+		End Sub
+	End Class
+End Namespace
